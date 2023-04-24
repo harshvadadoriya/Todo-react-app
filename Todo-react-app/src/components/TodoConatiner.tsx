601cf7f7bab393ef9d0cef343cfb1b1fc4c8794a
harshsimform <harsh.v@simformsolutions.com>
2023-04-24 17:21:52 +0530

    created reusable components

---
+++ Todo-react-app/src/components/TodoConatiner.tsx
@@ -0,0 +1,66 @@
+import React, { useState, useRef, useEffect } from "react";
+import DateComponent from "./DateComponent";
+import TodoComponent from "./TodoListComponent";
+
+const TodoContainer: React.FC = (): JSX.Element => {
+  const [showInput, setShowInput] = useState(false);
+  const inputRef: React.RefObject<HTMLInputElement> =
+    useRef<HTMLInputElement>(null);
+
+  const handlePlusClick = (): void => {
+    setShowInput(true);
+  };
+
+  useEffect(() => {
+    if (showInput) {
+      inputRef.current?.focus();
+    }
+    const handleEscape = (event: KeyboardEvent): void => {
+      if (event.key === "Escape") {
+        setShowInput(false);
+      }
+    };
+
+    document.addEventListener("keydown", handleEscape);
+
+    return () => {
+      document.removeEventListener("keydown", handleEscape);
+    };
+  }, [showInput]);
+
+  return (
+    <>
+      <section className="main">
+        <div className="mx-4 card">
+          <div className="container">
+            <DateComponent />
+            <div className="todo-container">
+              <TodoComponent text="Go for a walk" status="completed" />
+              <TodoComponent text="Try not to fall asleep" status="completed" />
+              <TodoComponent text="Go To Gym" status="pending" />
+              <TodoComponent text="Read an article" status="pending" />
+              <TodoComponent text="Watch 'Sherlock'" status="pending" />
+            </div>
+            {showInput ? (
+              <div className="inputText">
+                <input
+                  type="text"
+                  placeholder="Add Your Todo!"
+                  ref={inputRef}
+                />
+              </div>
+            ) : (
+              <button className="plus" onClick={handlePlusClick}>
+                <div className="add">
+                  <p>+</p>
+                </div>
+              </button>
+            )}
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default TodoContainer;
---
+++ Todo-react-app/src/components/TodoListComponent.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import pendingTodoImg from "../assets/task-pending.png";
+import successTodoImg from "../assets/task-completed.png";
+
+type TodoProps = {
+  text: string;
+  status: "completed" | "pending";
+};
+
+// React.Component used to define Class Component and it takes in the TodoProps interface as its generic type parameter.
+class TodoComponent extends React.Component<TodoProps> {
+  render(): React.ReactNode {
+    const { text, status } = this.props;
+    return (
+      <div className="todo">
+        <div className="todo-list">
+          <span className={status === "completed" ? "completed" : "pending"}>
+            {text}
+          </span>
+          <img
+            className="todo-status"
+            src={status === "completed" ? successTodoImg : pendingTodoImg}
+            alt={status === "completed" ? "completed" : "pending"}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default TodoComponent;
